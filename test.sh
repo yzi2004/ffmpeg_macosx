c0f1134c4e02cda2660371346ba2c859dee15f3c
Jerry <yzi@outlook.com>
2023-01-03 23:01:47 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 BASE="/Volumes/dev/ffmpeg"
 SOURCE="${BASE}/sources"
 BUILD="${BASE}/build"
@@ -10,11 +12,17 @@ export PATH=${TOOLS}/bin:$PATH
 export CC=clang 
 export PKG_CONFIG_PATH="${PREBUILT}/lib/pkgconfig"
 
-rm -rf $BUILD
-if [ $1 == "rebuild*" ]; then
-    rm -rf $PREBUILT
-fi
 
-if [ $1 == "rebuild_all*" ]; then
-    rm -rf $TOOLS
-fi
+$SOURCE/ffmpeg/configure --prefix=${PREBUILT} \
+   --extra-cflags="-fno-stack-check" \
+   --arch=arm64 \
+   --cc=/usr/bin/clang \
+   --enable-gpl \
+   --enable-version3 \
+   --pkg-config-flags=--static \
+   --disable-ffplay \
+   --enable-postproc \
+   --enable-nonfree \
+   --enable-runtime-cpudetect
+
+make -j 8
